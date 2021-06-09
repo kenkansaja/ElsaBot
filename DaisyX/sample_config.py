6037e4c16136b295790ac30b24e5162dd4858c14
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-09 13:43:18 +0700

    Update sample_config.py

--- DaisyX/sample_config.py
+++ DaisyX/sample_config.py
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "KEN KAN"
+    SUPPORT_CHAT = "musikkugroup"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
@@ -36,7 +36,7 @@ class Config(object):
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    SPAMWATCH_SUPPORT_CHAT = "@musikkuchannel"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
