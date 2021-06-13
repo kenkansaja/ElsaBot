d7b838f35016974541fb2fa497e6aed3354a7463
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-13 07:58:53 +0700

    Update afk.py

--- DaisyX/modules/afk.py
+++ DaisyX/modules/afk.py
@@ -57,14 +57,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} di sini!",
+                "{} kembali!",
+                "{} sekarang dalam obrolan!",
+                "{} sudah bangun!",
+                "{} kembali online!",
+                "{} akhirnya di sini!",
+                "Selamat datang kembali! {}",
+                "Di mana {}?\nDalam obrolan!",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
