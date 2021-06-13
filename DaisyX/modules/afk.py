8aaf581c905bf53bc19b3f9ad758129118308e41
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-13 07:55:15 +0700

    Create afk.py

--- DaisyX/modules/afk.py
+++ DaisyX/modules/afk.py
@@ -30,14 +30,14 @@ def afk(update: Update, context: CallbackContext):
         reason = args[1]
         if len(reason) > 100:
             reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+            notice = "\nAlasan afk Anda dipersingkat menjadi 100 karakter."
     else:
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("{} Sekarang AFK!{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -140,7 +140,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} Sedang afk.\nAlasan: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
