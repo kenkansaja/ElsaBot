6bb26ad7c670ad68590791b1ddaee047bb231ef9
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-13 08:45:17 +0700

    Update stickers.py

--- DaisyX/modules/stickers.py
+++ DaisyX/modules/stickers.py
@@ -452,10 +452,10 @@ def makepack_internal(
 
 
 __help__ = """
-• `/stickerid`*:* reply to a sticker to me to tell you its file ID.
-• `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
-• `/kang`*:* reply to a sticker to add it to your pack.
-• `/stickers`*:* Find stickers for given term on combot sticker catalogue
+• `/stickerid`*:* balas stiker ke saya untuk memberitahukan ID filenya.
+• `/getsticker`*:* balas stiker ke saya untuk mengunggah file PNG mentahnya.
+• `/kang`*:* membalas stiker untuk menambahkannya ke paket Anda.
+• `/stickers`*:* Temukan stiker untuk istilah tertentu di katalog stiker kombo.
 """
 
 __mod_name__ = "Stickers 🎉"
