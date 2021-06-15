a536d8c9f7513e0eb9e45e0f44bb41f3d29f63c2
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-15 10:59:51 +0700

    Update global_bans.py

--- DaisyX/modules/global_bans.py
+++ DaisyX/modules/global_bans.py
@@ -535,18 +535,12 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = f"""
-*Admins only:*
- • `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+*Hanya Admin :*
+`/antispam <on/off/yes/no>`*:* Akan mengaktifkan teknologi antispam kami atau mengembalikan pengaturan Anda saat ini.
 
-Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
-you and your groups by removing spam flooders as quickly as possible.
-*Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
-
-This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
-*What is SpamWatch?*
-SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
-Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
-*Note:* Users can appeal spamwatch bans at @SpamwatchSupport
+Anti-Spam, digunakan oleh pengembang bot untuk melarang spammer di semua grup. Ini membantu melindungi \
+Anda dan grup Anda dengan menghapus pembanjir spam secepat mungkin.
+*Catatan:* Pengguna dapat mengajukan banding atau melaporkan spammer di @{SUPPORT_CHAT}
 """
 
 GBAN_HANDLER = CommandHandler("gban", gban)
