aebdc04f79a4f109349a96d8e6ae21f9fe967888
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-08 20:50:30 +0700

    Update tagger.py

--- DaisyX/modules/tagger.py
+++ DaisyX/modules/tagger.py
@@ -37,19 +37,19 @@ async def _(event):
     await event.delete()
 
 @cutiepii(pattern="^/all ?(.*)")
-async def tagall(client, message):
-    await message.reply("`Processing.....`")
+async def_(event):
+    await event.get_reply_message("`Processing.....`")
     sh = get_text(message)
     if not sh:
         sh = "Hi!"
     mentions = ""
-    async for member in client.iter_chat_members(message.chat.id):
-        mentions += member.user.mention + " "
+    async for x in telethn.iter_participants(chat, 100):
+        mentions += x.user.mention + " "
     n = 4096
     kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
     for i in kk:
         j = f"<b>{sh}</b> \n{i}"
-        await client.send_message(message.chat.id, j, parse_mode="html")
+        await event.send_message(message.chat.id, j, parse_mode="html")
 
 
 __mod_name__ = "Tagger 🖇"
